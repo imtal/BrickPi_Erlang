c444be6833455daaa14abd097873177a4442ba5e
tjeerd <imtal@yolt.nl>
2014-08-25 06:53:06 +0200

    Uncommented error test

--- test/testset.erl
+++ test/testset.erl
@@ -9,10 +9,10 @@ start() ->
     {ok,_Pid} = brickpi:start(),
     ok = brickpi:set_sensor_type(?PORT_1,?TYPE_SENSOR_RAW),
     ok = brickpi:setup(),
-    %{error,parameter_overfow} = brickpi:set_motor_enable(6,1), % there are four ports
+    {error,parameter_overflow} = brickpi:set_motor_enable(6,1), % there are four ports
     ok = brickpi:set_motor_enable(?PORT_A,1),
     ok = brickpi:set_motor_speed(?PORT_A,200),
-    ok = brickpi:set_timeout(100000),
+    ok = brickpi:set_timeout(10000),
     ok = brickpi:update(),
     ok = brickpi:sleep(3000),
     ok = brickpi:set_motor_speed(?PORT_A,-200),
